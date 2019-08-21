b35929269a5e5a67ad9f81c63af4697ae23da232
Lovekarasimentaikoko <48247457+Lovekarasimentaikoko@users.noreply.github.com>
2019-08-21 18:02:08 +0900

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,21 @@
+$(function(){
+    $('#profile').hover(function() {
+    $('.profile-info').fadeIn();
+  },
+   function() {
+    $('.profile-info').fadeOut();
+  });
+    
+    $('#career').hover(function() {
+    $('.career-info').fadeIn();
+  },
+   function() {
+    $('.career-info').fadeOut();
+  });
+     $('#portfolio').hover(function() {
+    $('.portfolio-info').fadeIn();
+  },
+   function() {
+    $('.portfolio-info').fadeOut();
+  });
+});
